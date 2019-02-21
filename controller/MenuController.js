168324642f530c907f0acd101ea0f7605cd76896
Binaya Rimal <rimal.binaya@gmail.com>
2019-02-20 20:35:36 -0800

    testing completed

--- controller/MenuController.js
+++ controller/MenuController.js
@@ -52,4 +52,12 @@ module.exports = class MenuController {
     console.log("Thanks for using AddressBloc!");
     process.exit();
   }
+  getContactCount(){
+
+   return this.contacts.length;
+ }
+ remindMe(){
+   const reminder = "Learning is a life-long pursuit";
+   return reminder
+ }
 }
